37eeaa41b403ee70f9945f2aabc24f1fcc9ded47
Σndless <loli.con@qq.com>
2018-10-12 17:22:25 +0800

    Update pdf.go

--- pdf.go
+++ pdf.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"gopkg.in/gographics/imagick.v2/imagick"
 	"fmt"
+	"strconv"
 )
 
 func init() {
@@ -12,13 +13,31 @@ func init() {
 
 func main() {
 	if len(os.Args) <= 1 {
+		fmt.Fprintln(os.Stderr, "Usage:", os.Args[0], "<pdf-path>")
+		fmt.Fprintln(os.Stderr, "Usage:", os.Args[0], "<pdf-path>", "<page-index>")
 		os.Exit(1)
+	}
+
+	pdfPath := os.Args[1]
+
+	index := 1
+	var err error
+	if len(os.Args) >= 3 {
+		index, err = strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		if index < 1 {
+			fmt.Fprintln(os.Stderr, "index must > 0")
+			os.Exit(1)
+		}
 	}
 
 	mw := imagick.NewMagickWand()
 	defer mw.Destroy()
 
-	if err := mw.ReadImage(fmt.Sprintf("%s[0]", os.Args[1])); err != nil {
+	if err := mw.ReadImage(fmt.Sprintf("%s[%d]", pdfPath, index-1)); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
