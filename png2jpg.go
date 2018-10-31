b06082776701c2e117ac03e963a33af107d21ab2
Σndless <loli.con@qq.com>
2018-10-31 14:55:50 +0800

    Create png2jpg.go

---
+++ png2jpg.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"image/jpeg"
+	"image/png"
+	"path/filepath"
+)
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	if len(os.Args) <= 1 {
+		os.Exit(1)
+	}
+
+	fileName := os.Args[1]
+	fileNameWithExt := fileName
+	ext := filepath.Ext(fileName)
+	if ext == "" {
+		fileNameWithExt += ".png"
+	}
+
+	pngFile, err := os.Open(fileNameWithExt)
+	checkError(err)
+	defer pngFile.Close()
+
+	pngSrc, err := png.Decode(pngFile)
+	checkError(err)
+
+	jpgFile, err := os.Create(fileName + ".jpg")
+	checkError(err)
+
+	err = jpeg.Encode(jpgFile, pngSrc, &jpeg.Options{Quality: 100})
+	checkError(err)
+}
