4ed96331f26ab8c85f9e1d8836dfb7d89c931332
Σndless <loli.con@qq.com>
2019-03-07 17:38:36 +0800

    Update t.go

--- t.go
+++ t.go
@@ -8,29 +8,54 @@ import (
 )
 
 var (
-	lenUnixTimestamp     = len(strconv.Itoa(int(time.Now().Unix())))
-	lenUnixNanoTimestamp = len(strconv.Itoa(int(time.Now().UnixNano())))
+	lenUnixTimestamp = len(strconv.Itoa(int(time.Now().Unix())))
+	exp10            = [...]int64{
+		1,
+		1e1,
+		1e2,
+		1e3,
+		1e4,
+		1e5,
+		1e6,
+		1e7,
+		1e8,
+		1e9,
+	}
+	local, _ = time.LoadLocation("Asia/Shanghai")
 )
 
-func main() {
-	if len(os.Args) <= 1 {
-		os.Exit(1)
-	}
-
-	ts, err := strconv.Atoi(os.Args[1])
+func parseTimestamp(rawTs string) (time.Time, error) {
+	ts, err := strconv.ParseInt(rawTs, 10, 64)
 	if err != nil {
-		os.Exit(1)
+		// 寻找其他解析方式
+		if t, err := time.ParseInLocation("2006-01-02 15:04:05", rawTs, local); err == nil {
+			return t, nil
+		}
+		return time.ParseInLocation(time.RFC3339, rawTs, local)
 	}
 
+	switch {
+	case len(rawTs) >= lenUnixTimestamp:
+		_exp10 := exp10[len(rawTs)-lenUnixTimestamp]
+		return time.Unix(ts/_exp10, ts%_exp10), nil
+	default:
+		return time.Unix(ts, 0), nil
+	}
+}
+
+func main() {
 	var t time.Time
-	switch len(os.Args[1]) {
-	case lenUnixTimestamp:
-		t = time.Unix(int64(ts), 0)
-	case lenUnixTimestamp + 3:
-		t = time.Unix(int64(ts)/1000, int64(ts)%1000)
-	case lenUnixNanoTimestamp:
-		t = time.Unix(int64(ts)/int64(time.Second), int64(ts)%int64(time.Second))
+	if len(os.Args) <= 1 {
+		t = time.Now()
+	} else {
+		var err error
+		t, err = parseTimestamp(os.Args[1])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 	}
 
-	fmt.Println(t.Format("2006-01-02 15:04:05"))
+	t.In(local)
+	fmt.Println(t.Unix(), t.Format(time.RFC3339))
 }
